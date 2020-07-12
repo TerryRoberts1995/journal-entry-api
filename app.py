bd50b8e81850a8f0719b692f09ed8867d1341324
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-07-12 15:10:23 -0500

    added gitignore

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-# This is to check if everything is working correctly.
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from fask_marshmallow import Marshmallow
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
